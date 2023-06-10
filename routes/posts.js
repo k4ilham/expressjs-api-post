02a130737a7dca7634dec40123590380ce398d69
Ilham Maulana <k4ilham@gmail.com>
2023-06-11 00:45:00 +0700

    update data

--- routes/posts.js
+++ routes/posts.js
@@ -104,6 +104,52 @@ router.get('/', function (req, res) {
             })
         }
     })
-})
+});
+
+/**
+ * UPDATE POST
+ */
+ router.patch('/update/:id', [
+
+    //validation
+    body('title').notEmpty(),
+    body('content').notEmpty()
+
+], (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            errors: errors.array()
+        });
+    }
+
+    //id post
+    let id = req.params.id;
+
+    //data post
+    let formData = {
+        title: req.body.title,
+        content: req.body.content
+    }
+
+    // update query
+    connection.query(`UPDATE posts SET ? WHERE id = ${id}`, formData, function (err, rows) {
+        //if(err) throw err
+        if (err) {
+            return res.status(500).json({
+                status: false,
+                message: 'Internal Server Error',
+            })
+        } else {
+            return res.status(200).json({
+                status: true,
+                message: 'Update Data Successfully!'
+            })
+        }
+    })
+
+});
 
 module.exports = router;
